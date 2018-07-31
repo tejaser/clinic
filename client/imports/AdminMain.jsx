f610c1f4fb582cd2d4d5b705a81d304eefe5ec83
Tejas Pancholi <tejas.er@outlook.com>
2018-07-31 16:26:56 +0530

    refactored the code.

--- client/imports/AdminMain.jsx
+++ client/imports/AdminMain.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import Header from "/client/imports/Header";
-import Dashboard from "/client/imports/Dashboard";
 import { Route, Switch } from "react-router-dom";
+
+import Header from "/client/imports/Header";
+import Sidebar from "/client/imports/Sidebar";
+import Dashboard from "/client/imports/Dashboard";
 import NotFound from "./NotFound";
 
 export default class AdminMain extends Component {
@@ -9,10 +11,15 @@ export default class AdminMain extends Component {
     return (
       <div>
         <Header />
-        <Switch>
-          <Route exact path="/admin" component={Dashboard} />
-          <Route component={NotFound} />
-        </Switch>
+        <div className="container-fluid">
+          <div className="row">
+            <Sidebar />
+            <Switch>
+              <Route exact path="/admin" component={Dashboard} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </div>
       </div>
     );
   }
--- client/imports/Dashboard.jsx
+++ client/imports/Dashboard.jsx
@@ -1,28 +1,24 @@
 import React, { Component } from "react";
-import Sidebar from "/client/imports/Sidebar";
 
 export default class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <div className="container-fluid">
-          <div className="row">
-            <Sidebar />
-            <div role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-              <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-                <h1 className="h2">Dashboard</h1>
-                <div className="btn-toolbar mb-2 mb-md-0">
-                  <div className="btn-group mr-2">
-                    <button className="btn btn-sm btn-outline-secondary">Share</button>
-                    <button className="btn btn-sm btn-outline-secondary">Export</button>
-                  </div>
-                  <button className="btn btn-sm btn-outline-secondary dropdown-toggle">
-                    <span data-feather="calendar" />
-                    This week
-                  </button>
-                </div>
-              </div>
+      <div role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+          <h1 className="h2">Dashboard</h1>
+          <div className="btn-toolbar mb-2 mb-md-0">
+            <div className="btn-group mr-2">
+              <button className="btn btn-sm btn-outline-secondary">
+                Share
+              </button>
+              <button className="btn btn-sm btn-outline-secondary">
+                Export
+              </button>
             </div>
+            <button className="btn btn-sm btn-outline-secondary dropdown-toggle">
+              <span data-feather="calendar" />
+              This week
+            </button>
           </div>
         </div>
       </div>
